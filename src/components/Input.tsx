0aa043894adcc3e6848e9f2c98266288411c17c9
Benjamín Robles <benjamin.robles.a@mail.pucv.cl>
2024-04-13 18:15:09 -0400

    Corregir estructura Login

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -12,7 +12,7 @@ export const Input: React.FC<InputProp> = ({inputType, placeholder, className, o
         <input 
         type={inputType}
         placeholder={placeholder}
-        className={className + " block rounded-full border-0 py-2.5 pl-7 my-1.5 pr-5 md:pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:outline-none focus:ring-inset  sm:text-sm sm:leading-6"}
+        className={className + " block rounded-full border-0 py-2.5 pl-7 my-1.5 pr-5 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:outline-none focus:ring-inset"}
         onChange={onChange}
         />
     ) 
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -7,32 +7,25 @@ export default function Login() {
         <IonPage>
             <IonHeader>
                 <IonToolbar>
-                    <IonTitle>Login</IonTitle>
+                    <IonTitle>Iniciar Sesión</IonTitle>
                 </IonToolbar>
             </IonHeader>
             <IonContent>
-            <div className="flex justify-center items-center h-full flex-col w-30">
-        <div id="login" className="text-center w-32 h-full flex flex-col justify-center items-center">
-          <label>Inicia Sesión</label>
-          <div className="h-16"></div>
-          <div className="px-10">
-              <Input inputType={"text"} placeholder={"Usuario"}/>
-              <Input inputType={"password"} placeholder={"Contraseña"}/>
-
-              <div className="h-16"></div>
-              <div className="flex justify-center">
-                <div className="w-full flex flex-col mx-20 gap-3">
-                  <Button>Iniciar Sesión</Button>
-                  <Button>Registrarse</Button>
-
-
-                </div>  
-
-              </div>
-
-          </div>
-         </div>
-      </div>
+                <div className="flex justify-center items-center h-full flex-col w-30">
+                    <div id="login" className="text-center  h-full flex flex-col justify-center items-center">
+                        <h1 className="text-xl text-bold">Bienvenido</h1>
+                        <div className="h-16"></div>
+                            <Input inputType={"text"} placeholder={"Usuario"} />
+                            <Input inputType={"password"} placeholder={"Contraseña"} />
+                            <div className="h-6"></div>
+                            <div className="flex justify-center">
+                                <div className="w-full flex flex-col mx-20 gap-3">
+                                    <Button>Iniciar Sesión</Button>
+                                    <Button>Registrarse</Button>
+                                </div>
+                            </div>
+                    </div>
+                </div>
             </IonContent>
         </IonPage>
     );
