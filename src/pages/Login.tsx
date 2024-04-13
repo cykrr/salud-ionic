520bc573f2cd804979b1f6f5c7b32de10535de9b
seba <sebastian.jeria.l@mail.pucv.cl>
2024-04-13 17:47:21 -0400

    añadir vista login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,3 +1,6 @@
+import Input from '../components/Input';
+import Button from '../components/Button';
+
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 export default function Login() {
     return (
@@ -8,7 +11,28 @@ export default function Login() {
                 </IonToolbar>
             </IonHeader>
             <IonContent>
-                <p>Login</p>
+            <div className="flex justify-center items-center h-full flex-col w-30">
+        <div id="login" className="text-center w-32 h-full flex flex-col justify-center items-center">
+          <label>Inicia Sesión</label>
+          <div className="h-16"></div>
+          <div className="px-10">
+              <Input inputType={"text"} placeholder={"Usuario"}/>
+              <Input inputType={"password"} placeholder={"Contraseña"}/>
+
+              <div className="h-16"></div>
+              <div className="flex justify-center">
+                <div className="w-full flex flex-col mx-20 gap-3">
+                  <Button>Iniciar Sesión</Button>
+                  <Button>Registrarse</Button>
+
+
+                </div>  
+
+              </div>
+
+          </div>
+         </div>
+      </div>
             </IonContent>
         </IonPage>
     );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,9 @@
-module.exports = {
-    content: [    
-      './pages/**/*.{js,ts,jsx,tsx}',
-      './components/**/*.{js,ts,jsx,tsx}',
-    ],
-    darkMode: 'media',
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  };
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
